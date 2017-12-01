76995e30de529208a68918135dd5d79c22325e08
Madhumitha <mnimmagari@csc.com>
2017-12-01 17:13:27 +0530

    Advanced search with pagination

--- public/js/controllers/userController.js
+++ public/js/controllers/userController.js
@@ -1,19 +1,49 @@
 angular.module('userApp', ['angular-pagin-layout','layout']).controller('userController',['$http','$scope', function($http,$scope) {
     $scope.users=[];
     $scope.totalcount = 0;
+    $scope.searchfield='';
     $scope.itemsPerPage = 12;
-    getResultsPage(1);
 
     $scope.pageChanged = function(newPage) {
-        getResultsPage(newPage);
+        $scope.getResultsPage(newPage);
     };
 
-    function getResultsPage(newPage){
-        $http.get('/api/users?pagenumber='+newPage+'&itemsPerPage='+$scope.itemsPerPage).then(function(response){
+    $scope.getResultsPage = function(newPage){
+          var query = {
+         $or: [
+         {
+          'email': {
+            $regex: $scope.searchfield,
+            $options: 'i'
+          }
+        },
+        {
+          'name.first' :
+          {
+            $regex: $scope.searchfield,
+            $options: 'i'
+          }
+        },
+        {
+          'name.last' :
+          {
+            $regex: $scope.searchfield,
+            $options: 'i'
+          }
+        }
+        ]
+      }
+      var queryS = JSON.stringify(query);
+
+      userListTempUrl = '/api/users?pagenumber='+newPage+'&itemsPerPage='+$scope.itemsPerPage;
+      if($scope.searchfield){
+        userListTempUrl = '/api/users?pagenumber='+newPage+'&itemsPerPage='+$scope.itemsPerPage + '&query=' + encodeURIComponent(queryS);
+      }
+        $http.get(userListTempUrl).then(function(response){
          $scope.users = response.data.data;
          $scope.totalcount = response.data.totalCount;
      });
     };
-
+$scope.getResultsPage(1);
 
 }]);
